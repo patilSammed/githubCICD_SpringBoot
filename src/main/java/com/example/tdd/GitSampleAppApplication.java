8712b2af47255af051ac0f62e19fb4a816f16b07
patilSammed <psammed92@gmail.com>
2024-08-25 14:24:15 +0530

    welcome endpoint added

---
+++ src/main/java/com/example/tdd/GitSampleAppApplication.java
@@ -0,0 +1,27 @@
+package com.example.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitSampleAppApplication {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Application started");
+		SpringApplication.run(GitSampleAppApplication.class, args);
+		
+	}
+	
+	@GetMapping("/welcome")
+	public ResponseEntity<String> getWelcomeMessage()
+	{
+		return new ResponseEntity<String>("Welcome to GUTHUB CICD pipeline",HttpStatus.OK);
+	}
+
+}
